b0242101b98a98a7630de5d5dda1294014aa3fba
becrevex <brent@cygienesolutions.com>
2020-03-22 15:21:11 -0700

    working on fault tolerance (phase 1)

---
+++ generate_sscert.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+RED='\033[1;31m'
+BLUE='\033[1;34m'
+GREEN='\033[1;32m'
+YELLOW='\033[49;93m'
+NC='\033[0m' # No Color
+
+echo -e "${YELLOW}"
+echo -e "+---_-----------------------------------------------+"
+echo -e "|  Adversarial Informatics Combat SSCert Generator  |"
+echo -e "|                cygienesolutions.com               |"
+echo -e "|   [Usage]: ./generate_sscert.sh <TARGET> <PORT>   |"
+echo -e "+---------------------------------------------------+"
+echo -e "${NC}"
+if [ $# == 0 ] ; then
+    exit 1; fi
+
+#Generate self-signed cert
+openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out certificate.pem
+openssl x509 -text -noout -in certificate.pem
+openssl pkcs12 -inkey key.pem -in certificate.pem -export -out certificate.p12
+openssl pkcs12 -in certificate.p12 -noout -info
